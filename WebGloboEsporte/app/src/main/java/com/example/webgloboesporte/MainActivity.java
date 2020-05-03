1a562a1ce309914402810b8d432e66be5a00c8d6
clebernascimento <clebertds@gmail.com>
2020-05-03 08:59:09 -0400

    insert Project Globo Esporte

---
+++ WebGloboEsporte/app/src/main/java/com/example/webgloboesporte/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.webgloboesporte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebViewClient webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("https://globoesporte.globo.com/");
+
+        WebSettings webSettings = webView.getSettings();
+        //habitando o JavaScript
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
