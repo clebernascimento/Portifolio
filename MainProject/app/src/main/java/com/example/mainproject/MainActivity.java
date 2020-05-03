febc6617a2818d0d9729be778ec493b66b856e8a
clebernascimento <clebertds@gmail.com>
2020-05-03 10:02:53 -0400

    .

--- MainProject/app/src/main/java/com/example/mainproject/MainActivity.java
+++ MainProject/app/src/main/java/com/example/mainproject/MainActivity.java
@@ -87,15 +87,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-
-    @Override
-    public void onBackPressed() {
-        int count = getSupportFragmentManager().getBackStackEntryCount();
-        System.out.println(count);
-        if (count == 0) {
-            //super.onBackPressed();
-        } else {
-            //getSupportFragmentManager().popBackStack();
-        }
-    }
 }
