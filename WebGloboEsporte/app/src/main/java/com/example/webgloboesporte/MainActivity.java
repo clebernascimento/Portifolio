3f0d11d2d63b461e4329b22e1867bf5cb0c2fbf6
clebernascimento <clebertds@gmail.com>
2020-05-03 10:42:10 -0400

    onBackPressed

--- WebGloboEsporte/app/src/main/java/com/example/webgloboesporte/MainActivity.java
+++ WebGloboEsporte/app/src/main/java/com/example/webgloboesporte/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.webgloboesporte;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
@@ -30,11 +32,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-            webView.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sair do aplicativo");
+        builder.setMessage("Deseja sair do aplicativo?");
+        builder.setPositiveButton("Sim", (dialog, which) -> {
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.example.mainproject");
+            if (intent != null) {
+                startActivity(intent);
+                finish();
+            }
+        }).setNegativeButton("Não", null);
+        builder.show();
     }
 }
