4b8d8d68fb5e3d745b9ee838a04788449fa17ade
clebernascimento <clebertds@gmail.com>
2020-07-05 20:15:04 -0400

    onBackPressed IMC

--- CalculoIMC/app/src/main/java/com/example/calculoimc/MainActivity.java
+++ CalculoIMC/app/src/main/java/com/example/calculoimc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculoimc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        // code here to show dialog
+        Intent iMainProject = getPackageManager().getLaunchIntentForPackage("com.example.mainproject");
+        if (iMainProject != null) {
+            startActivity(iMainProject);
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            finish();
+        }
+        super.onBackPressed();  // optional depending on your needs
     }
 }
